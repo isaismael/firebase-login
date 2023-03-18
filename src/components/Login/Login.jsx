a1d9d3e17eb4e16496c53a590b3cd07cfa6a2ebc
Ismael Isa <ismael.isa02@gmail.com>
2023-03-18 16:46:34 -0300

    se agrego registro e inputs reetulizables, se hizo pruebra de validacion en registro

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.innerBox}>
+        <h1 className={styles.heading}>Login</h1>
+        <div className={styles.footer}>
+          <b className={styles.error}>Error</b>
+          <button>Login BTN</button>
+          <p>Crear Cuenta
+            <span><Link to='/Signup'> - ir</Link></span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
