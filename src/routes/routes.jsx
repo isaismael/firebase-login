388eda2dad77b18251ba31c554fc72a0e629d78e
Ismael Isa <ismael.isa02@gmail.com>
2023-03-18 13:36:30 -0300

    se enruto componentes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import { Home } from "../components/Home/Home";
 import { Login } from "../components/Login/Login";
+import { Signup } from "../components/Signup/Signup";
 
 export const MyRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const MyRoutes = () => {
         <Routes>
             <Route excat path="/" element={<Home/>}/>
             <Route excat path="/Login" element={<Login/>}/>
+            <Route excat path="/Signup" element={<Signup/>}/>
         </Routes>
     </Router>
   )
