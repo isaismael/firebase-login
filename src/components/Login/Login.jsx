b11c9f35b9232e20be1ceb5c32b1d91402db2b29
Ismael Isa <ismael.isa02@gmail.com>
2023-03-18 19:35:59 -0300

    se agrego login, se realizo prueba en registro de firebase, se agrego styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,58 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {InputControl} from '../InputControl/InputControl'
+import { auth } from '../../firebase'
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+
 import styles from "./Login.module.css";
+import { async } from "@firebase/util";
 
 export const Login = () => {
+  
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState({email: '', password: ''});
+
+  const [errorMsg, setErrorMsg] = useState([]);
+
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+  const Loguearse = () =>{
+    if(!values.email || !values.password){
+      setErrorMsg('Datos invalidos!')
+      return
+    }
+    setErrorMsg('');
+    setSubmitButtonDisabled(true);
+    signInWithEmailAndPassword(auth, values.email, values.password)
+    .then(async(res) => {
+      setSubmitButtonDisabled(false);
+      navigate('/');
+    })
+    .catch((err) => {
+      setSubmitButtonDisabled(false);
+      setErrorMsg(err.messsage);
+    });
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.innerBox}>
         <h1 className={styles.heading}>Login</h1>
+        <InputControl 
+        label='Email'
+        onChange={(event) => setValues((prev) => ({...prev,email:event.target.value}))}
+        placeholder='Ingrese su correo'
+        />
+        <InputControl 
+        label='Contraseña'
+        onChange={(event) => setValues((prev) => ({...prev,password:event.target.value}))}
+        placeholder='Ingrese su contraseña'
+        />
         <div className={styles.footer}>
-          <b className={styles.error}>Error</b>
-          <button>Login BTN</button>
+          <b className={styles.error}>{errorMsg}</b>
+          <button onClick={Loguearse} disabled={submitButtonDisabled}>Login</button>
           <p>Crear Cuenta
             <span><Link to='/Signup'> - ir</Link></span>
           </p>
